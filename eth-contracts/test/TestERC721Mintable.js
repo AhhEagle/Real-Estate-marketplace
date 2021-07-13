9da56734829155586f7bc20cbb9c54c8498f5ec1
AhhEagle <dim2though@gmail.com>
2021-07-13 19:03:54 +0100

    Implement some tests

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -10,14 +10,23 @@ contract('TestERC721Mintable', accounts => {
             this.contract = await ERC721MintableComplete.new({from: account_one});
 
             // TODO: mint multiple tokens
+            await contract.mint(account_one, 1, { from: account_one });
+            await contract.mint(account_one, 2, { from: account_one });
+            await contract.mint(account_two, 3, { from: account_one });
+            await contract.mint(account_two, 4, { from: account_one });
+
         })
 
         it('should return total supply', async function () { 
-            
+            let totalSupply = await contract.totalSupply.call();
+            assert.equal(4, totalSupply, "Total token minted should be four");
         })
 
         it('should get token balance', async function () { 
-            
+            let balanceOne = await contract.balanceOf.call(account_one);
+            assert.equal(2, balanceOne, "account_one should have two tokens on its own.");
+            let balanceTwo = await contract.balanceOf.call(account_two);
+            assert.equal(2, balanceTwo, "account_two should have two token on its own.");
         })
 
         // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
